d5c554bbd901e00f0dcf6871ddf3b6840bea0047
John West <contactjohnwest@gmail.com>
2020-05-23 11:40:40 -0700

    finished chapter 2

---
+++ HeadFirstChpt2/src/DrumKitTestDrive.java
@@ -0,0 +1,26 @@
+public class DrumKitTestDrive {
+    public static void main(String[] args) {
+        DrumKit d = new DrumKit();
+        d.playSnare();
+        d.playTopHat();
+        d.snare = false;
+        if (d.snare) {
+            d.playSnare();
+        }
+    }
+}
+
+class DrumKit {
+    boolean topHate = true;
+    boolean snare = true;
+
+    void playSnare() {
+        System.out.println("bang bang ba-bang");
+    }
+
+    void playTopHat() {
+        System.out.println("ding ding da-ding");
+    }
+}
+
+
