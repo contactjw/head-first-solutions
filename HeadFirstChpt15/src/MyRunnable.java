1e8fc6c55ac67d875f36ddc94cd6a4cb2e439d3d
John West <contactjohnwest@gmail.com>
2020-06-08 09:34:26 -0700

    finished chapter 15

---
+++ HeadFirstChpt15/src/MyRunnable.java
@@ -0,0 +1,30 @@
+public class MyRunnable implements Runnable {
+    public void run() {
+        go();
+    }
+
+    public void go() {
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException ex) {
+            ex.printStackTrace();
+        }
+
+        doMore();
+    }
+
+    public void doMore() {
+        System.out.println("Top o' the stack!");
+    }
+}
+
+
+class ThreadTestDrive {
+    public static void main(String[] args) {
+        Runnable theJob = new MyRunnable();
+        Thread t = new Thread(theJob);
+
+        t.start();
+        System.out.println("Back in main.");
+    }
+}
